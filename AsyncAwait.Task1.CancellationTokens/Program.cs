63ec753d8917a10f22752f020c30b45abe990f26
Andrei_Bertosh <andrei.bertosh@gmail.com>
2021-11-04 10:51:37 +0300

    init request

---
+++ AsyncAwait.Task1.CancellationTokens/Program.cs
@@ -0,0 +1,80 @@
+﻿/*
+ * Изучите код данного приложения для расчета суммы целых чисел от 0 до N, а затем
+ * измените код приложения таким образом, чтобы выполнялись следующие требования:
+ * 1. Расчет должен производиться асинхронно.
+ * 2. N задается пользователем из консоли. Пользователь вправе внести новую границу в процессе вычислений,
+ * что должно привести к перезапуску расчета.
+ * 3. При перезапуске расчета приложение должно продолжить работу без каких-либо сбоев.
+ */
+
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncAwait.Task1.CancellationTokens
+{
+    class Program
+    {
+        private static Task taskCalculator;
+        private static CancellationTokenSource tokenSource = new CancellationTokenSource();
+
+        /// <summary>
+        /// The Main method should not be changed at all.
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mentoring program L2. Async/await.V1. Task 1");
+            Console.WriteLine("Calculating the sum of integers from 0 to N.");
+            Console.WriteLine("Use 'q' key to exit...");
+            Console.WriteLine();
+
+            Console.WriteLine("Enter N: ");
+
+            string input = Console.ReadLine();
+            while (input.Trim().ToUpper() != "Q")
+            {
+                if (int.TryParse(input, out int n))
+                {
+                    var taskCalc = new Task(() =>
+                    {
+                        CalculateSum(n);
+                    });
+                    taskCalc.Start();
+                }
+                else
+                {
+                    Console.WriteLine($"Invalid integer: '{input}'. Please try again.");
+                    Console.WriteLine("Enter N: ");
+                }
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine("Press any key to continue");
+            Console.ReadLine();
+        }
+
+        private static void CalculateSum(int n)
+        {
+            if ((taskCalculator != null) && !taskCalculator.IsCompleted)
+            {
+                tokenSource.Cancel();
+                taskCalculator.Wait();
+                tokenSource = new CancellationTokenSource();
+            }
+
+            var token = tokenSource.Token;
+
+            taskCalculator =  Task.Run(async () =>
+            {
+                var sum = await Calculator.Calculate(n, token);
+                Console.WriteLine($"Sum for {n} = {sum}.");
+                Console.WriteLine();
+                Console.WriteLine("Enter N: ");
+            }, token).ContinueWith(o => Console.WriteLine($"Sum for {n} cancelled..."), TaskContinuationOptions.OnlyOnCanceled);
+
+            Console.WriteLine($"The task for {n} started... Enter N to cancel the request:");
+        }
+    }
+}
---
+++ AsyncAwait.Task2.CodeReviewChallenge/Middleware/StatisticMiddleware.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using AsyncAwait.Task2.CodeReviewChallenge.Headers;
+using CloudServices.Interfaces;
+using Microsoft.AspNetCore.Http;
+
+namespace AsyncAwait.Task2.CodeReviewChallenge.Middleware
+{
+    public class StatisticMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        private readonly IStatisticService _statisticService;
+
+        public StatisticMiddleware(RequestDelegate next, IStatisticService statisticService)
+        {
+            _next = next;
+            _statisticService = statisticService ?? throw new ArgumentNullException(nameof(statisticService));
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {   
+            string path = context.Request.Path;
+                                                    // to async
+            Task staticRegTask = Task.Run(
+               async () =>  await _statisticService.RegisterVisitAsync(path)).ContinueWith(async o =>
+               {
+                   var result = await _statisticService.GetVisitsCountAsync(path);
+                   context.Response.Headers.Add(CustomHttpHeaders.TotalPageVisits, result.ToString());
+               }, TaskContinuationOptions.OnlyOnRanToCompletion);
+
+            Console.WriteLine(staticRegTask.Status); // just for debugging purposes
+
+            //Task staticRegTask = Task.Run(
+            //    () => _statisticService.RegisterVisitAsync(path)
+            //    .ConfigureAwait(false)
+            //    .GetAwaiter().OnCompleted(UpdateHeaders));
+
+            //Console.WriteLine(staticRegTask.Status); // just for debugging purposes
+            
+            //void UpdateHeaders()
+            //{
+            //    context.Response.Headers.Add(
+            //        CustomHttpHeaders.TotalPageVisits,
+            //        _statisticService.GetVisitsCountAsync(path).GetAwaiter().GetResult().ToString());
+            //}
+
+            Thread.Sleep(3000); // without this the statistic counter does not work
+            await _next(context);
+        }
+    }
+}
